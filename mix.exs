4146333a4a55baa47fe907c65826e3d35c1cb158
Chris Caragianis <chriscaragianis@gmail.com>
2018-10-12 14:02:00 -0400

    Revert "add test watcher"
    This reverts commit 47f624d5ad053038f95b029e999d62b4bfb0cf40.

--- mix.exs
+++ mix.exs
@@ -21,8 +21,7 @@ defmodule LoggerLogstashBackend.Mixfile do
       {:earmark, "~> 1.0.3", only: :dev},
       {:ex_doc, "~> 0.14.5", only: :dev},
       {:exjsx, "~> 3.2.1"},
-      {:timex, "~> 3.1.8"},
-      {:mix_test_watch, "~> 0.8", only: :dev, runtime: false}
+      {:timex, "~> 3.1.8"}
     ]
   end
 
